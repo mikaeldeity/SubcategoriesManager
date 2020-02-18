6b99832dd2eda939fdc4095c72c9a5bf22455b24
Mikael Santrolli <mikael.santrolli@gmail.com>
2020-02-18 18:05:12 +0000

    Update App.cs

--- App.cs
+++ App.cs
@@ -14,7 +14,7 @@ namespace SubcategoriesMerger
             PushButtonData b4Data = new PushButtonData("Manage", "Manage", thisAssemblyPath, "SubcategoriesMerger.Merge");
             PushButton pb4 = ribbonPanel.AddItem(b4Data) as PushButton;
             pb4.ToolTip = "Merge or replace Subcategories in the Project.";
-            BitmapImage pb4Image = new BitmapImage(new Uri("pack://application:,,,/SubcategoriesMerger;component/Resources/SubcategoriesMerger.png"));
+            BitmapImage pb4Image = new BitmapImage(new Uri("pack://application:,,,/SubcategoriesMerger;component/Resources/SubcategoriesManager.png"));
             pb4.LargeImage = pb4Image;
         }
 
